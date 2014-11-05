8e341613b7fc980123cea3d2e384411bec0c3ee9
Dave Gurnell <dave@untyped.com>
2014-11-05 17:43:02 +0000

    More work on the home page

--- src/js/site.js
+++ src/js/site.js
@@ -1,3 +1,9 @@
+// $(".navbar-toggle").click(function(evt) {
+//   evt.preventDefault();
+//   evt.stopPropagation();
+//   $(this).parents(".navbar").toggleClass("navbar-expanded");
+// });
+
 /*
 Currently unused:
 
@@ -19,144 +25,148 @@ function queryKeys() {
 }
 */
 
-function queryValues(name) {
-  var search = window.location.search;
+// function queryValues(name) {
+//   var search = window.location.search;
 
-  if(!search) {
-    return [];
-  }
+//   if(!search) {
+//     return [];
+//   }
 
-  if(search[0] == '?') {
-    search = search.substring(1);
-  }
+//   if(search[0] == '?') {
+//     search = search.substring(1);
+//   }
 
-  return _.chain(search.split("&")).
-    map(function(item) { return item.split("="); }).
-    filter(function(item) { return item[0] == name; }).
-    map(function(item) { return decodeURIComponent(item[1]); }).
-    value();
-}
+//   return _.chain(search.split("&")).
+//     map(function(item) { return item.split("="); }).
+//     filter(function(item) { return item[0] == name; }).
+//     map(function(item) { return decodeURIComponent(item[1]); }).
+//     value();
+// }
 
-$("#services a").on("click", function(evt) {
-  var link = $(this);
-  var target = $(link.attr("href"));
+// $("#services a").on("click", function(evt) {
+//   var link = $(this);
+//   var target = $(link.attr("href"));
 
-  $('html, body').animate({ scrollTop: target.offset().top }, 250);
+//   $('html, body').animate({ scrollTop: target.offset().top }, 250);
 
-  evt.preventDefault();
-});
+//   evt.preventDefault();
+// });
 
-$('#enquiries-form').each(function() {
-  var form = $(this);
+// $('#enquiries-form').each(function() {
+//   var form = $(this);
 
-  _.each(queryValues("course"), function(item) {
-    form.
-      find('input[name=course-' + item + ']').
-      attr("checked", "checked");
+//   _.each(queryValues("course"), function(item) {
+//     form.
+//       find('input[name=course-' + item + ']').
+//       attr("checked", "checked");
+//   });
+
+//   $("[name=name]").focus();
+
+//   form.find("button[type=submit]").on('click', function(evt) {
+//     evt.preventDefault();
+//     window.ga('send', 'event', 'booking', 'submit', { hitCallback: function () {
+//       form.submit();
+//     }});
+//   });
+// });
+
+// $('#contact-form').each(function() {
+//   var form = $(this);
+
+//   _.each(queryValues("subject"), function(subject) {
+//     form.
+//       find('input[name=subject]').
+//       attr("value", subject);
+//   });
+
+//   $("[name=name]").focus();
+
+//   form.find("button[type=submit]").on('click', function(evt) {
+//     evt.preventDefault();
+//     window.ga('send', 'event', 'contact', 'submit', { hitCallback: function () {
+//       form.submit();
+//     }});
+//   });
+// });
+
+// $("a[href*=underscoreconsulting]").on("click", function(evt) {
+//   evt.preventDefault();
+
+//   var url = $(this).attr("href");
+//   window.ga('send', 'event', 'outbound', 'click', url, { hitCallback: function () {
+//     document.location = url;
+//   }});
+// });
+
+$("html.home").each(function() {
+  var newScrollPos = 0;
+  var oldScrollPos = 0;
+  var heroHeight = $(".home-hero").height();
+  var standardNavbar = $(".navbar-standard");
+
+  $(window).on("scroll", function() {
+    oldScrollPos = newScrollPos;
+    newScrollPos = $(window).scrollTop();
+
+    if(newScrollPos > heroHeight && oldScrollPos <= heroHeight) {
+      standardNavbar.addClass("active");
+    } else if(newScrollPos < heroHeight && oldScrollPos >= heroHeight) {
+      standardNavbar.removeClass("active");
+    }
   });
 
-  $("[name=name]").focus();
+  $(".home-blog").each(function () {
+    var root        = $(this);
+    var prevPaddle  = root.find(".home-blog-paddle-prev");
+    var nextPaddle  = root.find(".home-blog-paddle-next");
+    var prevLink    = root.find(".home-blog-link-prev");
+    var nextLink    = root.find(".home-blog-link-next");
+    var postWrapper = root.find(".home-blog-posts");
+    var posts       = postWrapper.find(".blog-post");
+    var curr        = 0;
 
-  form.find("button[type=submit]").on('click', function(evt) {
-    evt.preventDefault();
-    window.ga('send', 'event', 'booking', 'submit', { hitCallback: function () {
-      form.submit();
-    }});
-  });
-});
-
-$('#contact-form').each(function() {
-  var form = $(this);
-
-  _.each(queryValues("subject"), function(subject) {
-    form.
-      find('input[name=subject]').
-      attr("value", subject);
-  });
-
-  $("[name=name]").focus();
-
-  form.find("button[type=submit]").on('click', function(evt) {
-    evt.preventDefault();
-    window.ga('send', 'event', 'contact', 'submit', { hitCallback: function () {
-      form.submit();
-    }});
-  });
-});
-
-$("a[href*=underscoreconsulting]").on("click", function(evt) {
-  evt.preventDefault();
-
-  var url = $(this).attr("href");
-  window.ga('send', 'event', 'outbound', 'click', url, { hitCallback: function () {
-    document.location = url;
-  }});
-});
-
-$(".training-directory tr").on("click", function(evt) {
-  var url = $(this).data("courseUrl");
-  if(url) window.location = url;
-});
-
-$(".training-directory tr a").on("click", function(evt) {
-  evt.stopPropagation();
-});
-
-
-$(".home-blog").each(function () {
-  var root        = $(this);
-  var prevPaddle  = root.find(".home-blog-paddle-prev");
-  var nextPaddle  = root.find(".home-blog-paddle-next");
-  var postWrapper = root.find(".home-blog-posts");
-  var posts       = postWrapper.find(".blog-post");
-  var curr        = 0;
-
-  function resizePaddles(post) {
-    postWrapper.innerHeight(post.height());
-    prevPaddle.innerHeight(post.height());
-    nextPaddle.innerHeight(post.height());
-  }
-
-  function showPost(next, callback) {
-    callback = callback || function() {};
-
-    var prev = curr;
-
-    while(next < 0) {
-      next += posts.length;
+    function resizePaddles(post) {
+      postWrapper.innerHeight(post.height());
+      prevPaddle.innerHeight(post.height());
+      nextPaddle.innerHeight(post.height());
     }
 
-    while(next >= posts.length) {
-      next -= posts.length;
-    }
+    function showPost(next) {
+      var prev = curr;
 
-    curr = next;
+      if(posts.length > 0) {
+        while(next < 0) {
+          next += posts.length;
+        }
 
-    var prevPost = $(posts.get(prev));
-    var nextPost = $(posts.get(next));
+        while(next >= posts.length) {
+          next -= posts.length;
+        }
+      }
 
-    prevPost.fadeOut("fast", function() {
-      resizePaddles(nextPost);
-      nextPost.fadeIn("fast", function() {
-        callback();
+      curr = next;
+
+      var prevPost = $(posts.get(prev));
+      var nextPost = $(posts.get(next));
+
+      prevPost.fadeOut("fast", function() {
+        resizePaddles(nextPost);
+        nextPost.fadeIn("fast");
       });
-    });
-  }
-
-  function gotoPost(evt) {
-    console.log("THIS", this);
-
-    var url = $(this).data("postUrl");
-
-    console.log("URL", url);
-
-    if(url) {
-      window.location = url;
     }
-  }
 
-  prevPaddle.on("click", function(evt) { showPost(curr - 1); });
-  nextPaddle.on("click", function(evt) { showPost(curr + 1); });
-  postWrapper.on("click", ".blog-post", gotoPost);
-  showPost(curr);
+    function gotoPost(evt) {
+      var url = $(this).data("postUrl");
+
+      if(url) {
+        window.location = url;
+      }
+    }
+
+    prevPaddle.add(prevLink).on("click", function(evt) { showPost(curr - 1); });
+    nextPaddle.add(nextLink).on("click", function(evt) { showPost(curr + 1); });
+    postWrapper.on("click", ".blog-post", gotoPost);
+    showPost(curr);
+  });
 });
